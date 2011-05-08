7a63a682e6da6d513a22f8d909c9acc38245a837
icomeinpieces <piattjustin.jj@gmail.com>
2011-05-07 22:33:32 -0600

    added dropping dirt

--- src/com/icomeinpieces/UndroppableDroppables/UDBlockListener.java
+++ src/com/icomeinpieces/UndroppableDroppables/UDBlockListener.java
@@ -14,14 +14,14 @@ public class UDBlockListener extends BlockListener{
 	    plugin = instance;
 	}
 	public void onBlockBreak(BlockBreakEvent event) {
-		if (event.getBlock().getTypeId() == 47)
+		if (event.getBlock().getTypeId() == 47) //bookcases drop bookcases not nothing
 		{
 	        Location locy = new Location(event.getBlock().getWorld(), event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ(), 0.0F, 0.0F);
 	        event.getBlock().getWorld().dropItem(locy, new ItemStack(47, 1, (short) 1));
 	        event.getBlock().setType(Material.AIR);
 	        event.setCancelled(true);
 		}
-		if (event.getBlock().getTypeId() == 53)
+		if (event.getBlock().getTypeId() == 53) //wooden stairs drops wooden stairs not wood
 		{
 	        Location locy = new Location(event.getBlock().getWorld(), event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ(), 0.0F, 0.0F);
 	
@@ -29,7 +29,7 @@ public class UDBlockListener extends BlockListener{
 	        event.getBlock().setType(Material.AIR);
 	        event.setCancelled(true);
 		}
-		if (event.getBlock().getTypeId() == 67)
+		if (event.getBlock().getTypeId() == 67) // stone stairs drops stone stairs not cobblestone
 		{
 	        Location locy = new Location(event.getBlock().getWorld(), event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ(), 0.0F, 0.0F);
 	
@@ -37,28 +37,35 @@ public class UDBlockListener extends BlockListener{
 	        event.getBlock().setType(Material.AIR);
 	        event.setCancelled(true);
 		}
-		if (event.getBlock().getTypeId() == 89)
+		if (event.getBlock().getTypeId() == 89) //glowstone drops glowstone not dust
 		{
 	        Location locy = new Location(event.getBlock().getWorld(), event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ(), 0.0F, 0.0F);
 	        event.getBlock().getWorld().dropItem(locy, new ItemStack(89, 1, (short) 1));
 	        event.getBlock().setType(Material.AIR);
 	        event.setCancelled(true);
 		}
-		if (event.getBlock().getTypeId() == 20)
+		if (event.getBlock().getTypeId() == 20) //glass drops glass not nothing
 		{
 	        Location locy = new Location(event.getBlock().getWorld(), event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ(), 0.0F, 0.0F);
 	        event.getBlock().getWorld().dropItem(locy, new ItemStack(20, 1, (short) 1));
 	        event.getBlock().setType(Material.AIR);
 	        event.setCancelled(true);
 		}
-		if (event.getBlock().getTypeId() == 79)
+		if (event.getBlock().getTypeId() == 79) //ice drops ice not water in it's place
 		{
 	        Location locy = new Location(event.getBlock().getWorld(), event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ(), 0.0F, 0.0F);
 	        event.getBlock().getWorld().dropItem(locy, new ItemStack(79, 1, (short) 1));
 	        event.getBlock().setType(Material.AIR);
 	        event.setCancelled(true);
 		}
-		if (event.getBlock().getTypeId() == 8)
+		if (event.getBlock().getTypeId() == 2) //grass drops grass not dirt add occasional seeds?
+		{
+	        Location locy = new Location(event.getBlock().getWorld(), event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ(), 0.0F, 0.0F);
+	        event.getBlock().getWorld().dropItem(locy, new ItemStack(2, 1, (short) 1));
+	        event.getBlock().setType(Material.AIR);
+	        event.setCancelled(true);
+		}
+		if (event.getBlock().getTypeId() == 8) //handles the water that generates after an ice block is broken
 		{
 			event.getBlock().setType(Material.AIR);
 		}
